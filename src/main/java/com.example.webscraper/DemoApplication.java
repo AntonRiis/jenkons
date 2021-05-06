876c034d110322c439033a74d1945f43b181d8a0
Anton <anton.riis@gmail.com>
2021-05-06 15:47:21 +0200

    gvch2

--- src/main/java/com.example.webscraper/DemoApplication.java
+++ src/main/java/com.example.webscraper/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebscraperApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebscraperApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
